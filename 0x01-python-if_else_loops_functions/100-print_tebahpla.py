64afc53b309f6956d1db286e2a0b1c8f1d13058f
MonaaEid <monaa_eid@hotmail.com>
2023-09-05 22:19:37 +0300

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    print("{:c}".format(i if i % 2 == 0 else i + 32), end="")
+for i in reversed(range(97, 123)):
+    print("{:c}".format(i if (i % 2 == 0) else (i - 32)), end='')
