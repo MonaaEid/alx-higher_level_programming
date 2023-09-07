c17d18d6e8352e7b047559247230e13e7cd48697
MonaaEid <monaa_eid@hotmail.com>
2023-09-07 16:13:50 +0300

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
-def main():
+if __name__ == "__main__":
+    import sys
     num_args = len(sys.argv)
 
     if num_args == 1:
@@ -12,5 +12,5 @@ def main():
 
     for i in range(1, num_args):
         print("{}: {}".format(i, sys.argv[i]))
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+    #main()
