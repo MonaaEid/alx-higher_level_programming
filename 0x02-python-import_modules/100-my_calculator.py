639739120c23c193e943722953bdf73c2a0db553
MonaaEid <monaa_eid@hotmail.com>
2023-09-07 17:32:01 +0300

    test

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+import sys
+
+def main():
+    num_args = len(sys.argv)
+    if num_args != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+        if operator not in "+-*/":
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        operations = {
+            "+": add,           
+            "-": sub,
+            "*": mul,
+            "/": div
+
+            }
+        func = operations[operator]
+
+                                                                                                                                result = func(a, b)
+                                                                                                                                print("{} {} {} = {}".format(a, operator, b, result))
+                                                                                                                        if __name__ == "__main__":
+                                                                                                                            main()
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import importlib
+
+
 hidden_4 = importlib.import_module("hidden_4")
+
+
 def main():
     names = dir(hidden_4)
 
@@ -9,6 +13,7 @@ def main():
     for name in names:
         if not name.startswith("__"):
             print(name)
+
 
 if __name__ == "__main__":
     main()
