bde59655b95410bd25d9e2bda11f93fb21a2feff
MonaaEid <monaa_eid@hotmail.com>
2023-09-09 17:35:54 +0300

    test

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer to a Python object
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, i;
+	PyObject *item;
+	PyListObject *list;
+
+	if (PyList_Check(p))
+	{
+		list = (PyListObject *)p;
+		size = PyList_Size(p);
+		printf("[*] Size of the Python List = %ld\n", size);
+		printf("[*] Allocated = %ld\n", list->allocated);
+		for (i = 0; i < size; i++)
+		{
+			item = PyList_GetItem(p, i);
+			printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		}
+
+	}
+}
