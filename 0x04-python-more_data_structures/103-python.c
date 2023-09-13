5e408e63f4bcbbb78355cf08eff85936b5ea6f4c
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-09-14 02:55:36 +0300

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,24 +8,17 @@
  */
 void print_python_list(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
-
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
+	Py_ssize_t i, size;
+	PyObject *item;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->ob_base.ob_size);
+	for (i = 0, size = PyList_Size(p); i < size; i++)
 	{
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-	}
+		item = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
+    	}
 }
 /**
  * print_python_bytes - Prints bytes information
@@ -35,35 +28,27 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	char *string;
-	long int size, i, limit;
+
+	Py_ssize_t i, size;
+	char *buffer;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
+    	{
+        	printf("  [ERROR] Invalid Bytes Object\n");
+        	return;
+    	}
 
-	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
+	size = ((PyVarObject *)p)->ob_size;
+    	buffer = ((PyBytesObject *)p)->ob_sval;
 
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
+    	printf("  trying string: %s\n", buffer);
 
-	if (size >= 10)
-		limit = 10;
-	else
-		limit = size + 1;
-
-	printf("  first %ld bytes:", limit);
-
-	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
-		else
-			printf(" %02x", 256 + string[i]);
-
+	if (size > 10)
+		size = 10;
+	printf("  first %ld bytes:", size);
+	for (i = 0; i < size; i++)
+		printf(" %02x", buffer[i] & 0xff);
 	printf("\n");
 }
-
