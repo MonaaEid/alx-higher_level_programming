73c9d2f255bfcd86e5678a2b72adf248b6d50385
MonaaEid <monaa_eid@hotmail.com>
2023-09-27 01:18:29 +0300

    test

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -90,28 +90,14 @@ class Square:
         return self.size ** 2
 
     def my_print(self):
-         """Prints the square with the character # and spaces based on its position.
-
+        """Prints the square with the character # and
+        spaces based on its position
         If the size is equal to 0, it prints an empty line.
-
         """
         if self.__size == 0:
-            print("")
+            print()
             return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
-
-    def __str__(self):
-        """Define the print() representation of a Square."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+        for _ in range(self.position[1]):
+            print()
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
