2490cf87f39b261f242d96a1af7c7ba2baae29fb
MonaaEid <monaa_eid@hotmail.com>
2023-09-27 02:24:21 +0300

    test

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
+
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError("radius must be a number")
+            else:
+                self.__radius = int(radius)
+        else:
+            self.__radius = radius
