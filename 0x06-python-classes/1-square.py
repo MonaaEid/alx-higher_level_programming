ff8ca378e6c8aaee6f617ebeb4b4b9ace14701c8
MonaaEid <monaa_eid@hotmail.com>
2023-09-26 20:07:12 +0300

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size):
         """ initialize square
         Args:
-        size (int) :size of the square
+        size (int):size of the square
         """
         self.__size = size
 
