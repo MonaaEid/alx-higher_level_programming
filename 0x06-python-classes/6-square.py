01716507c80143d5627cb4021b30d79ea0d72944
MonaaEid <monaa_eid@hotmail.com>
2023-09-27 03:14:38 +0300

    test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,8 +54,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
@@ -79,12 +78,12 @@ class Square:
             ValueError: If value contains negative integers.
 
         """
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(i, int) for i in value) or \
+                not all(i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """Calculates the area of the square.
