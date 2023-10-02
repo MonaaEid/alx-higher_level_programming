024d8526ab701f9d66fd667c5644024589e53068
MonaaEid <monaa_eid@hotmail.com>
2023-10-02 03:50:47 +0300

    update

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -12,14 +12,26 @@ def lazy_matrix_mul(m_a, m_b):
         m_a: list of integers or floats
         m_b: list of integers or floats
     """
-    if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("m_a and m_b must be a list")
-    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a and m_b must be a list of lists")
-    if not all(len(row) > 0 for row in m_a) or not all(len(row) > 0 for row in m_b):
-        raise ValueError("m_a and m_b can't be empty")
-    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_a and each row of m_b must be of the same size")
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if not all(len(row) > 0 for row in m_a):
+        raise ValueError("m_a can't be empty")
+    if not all(len(row) > 0 for row in m_b):
+        raise ValueError("m_b can't be empty")
+
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
