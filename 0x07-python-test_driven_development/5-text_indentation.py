08729817916da095bb457478e0899e2bbe0504af
MonaaEid <monaa_eid@hotmail.com>
2023-10-04 01:43:19 +0300

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,11 +11,9 @@ def text_indentation(text):
     """
     if type(text) != str:
         raise TypeError("text must be a string")
-    text = text.strip()
-    for i in range(len(text)):
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print(text[i])
-            print()
-        else:
-            print(text[i], end="")
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
 
