a24f914de151d0315a4edafc7a764a0f947124c5
MonaaEid <monaa_eid@hotmail.com>
2023-10-09 21:23:01 +0300

    test

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,4 +5,9 @@ This is is_kind_of_class module
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Args:
+        obj: object
+        a_class: class
+    """
     return isinstance(obj, a_class)
