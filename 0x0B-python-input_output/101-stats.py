e3b045ea5126f45f9a6f7fef3bf6c04cdeb48311
MonaaEid <monaa_eid@hotmail.com>
2023-10-11 01:00:40 +0300

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,26 +13,40 @@ def print_statistics(total_size, status_codes):
             print("{}: {}".format(key, status_codes[key]))
 
 def main():
-    total_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
+
     try:
         for line in sys.stdin:
-            count += 1
-            words = line.split()
-            try:
-                size = int(words[-1])
-                total_size += size
-                code = words[-2]
-                if code in status_codes:
-                    status_codes[code] += 1
-            except Exception as e:
-                pass
-            if count % 10 == 0:
-                print_statistics(total_size, status_codes)
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_codes)
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
 
 if __name__ == '__main__':
     main()
