5ffb923435927959d6774e58946526b9a1fac536
MonaaEid <monaa_eid@hotmail.com>
2023-10-11 00:25:18 +0300

    test

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 """
-from_json_string module
+Student class
 """
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in
+                    self.__dict__.items() if key in attrs}
