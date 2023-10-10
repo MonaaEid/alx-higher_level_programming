6b5f5b8868bf5f418398b2b633143985cca2f03f
MonaaEid <monaa_eid@hotmail.com>
2023-10-11 00:22:21 +0300

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 """
-from_json_string module
+Student Class
 """
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+        return self.__dict__
