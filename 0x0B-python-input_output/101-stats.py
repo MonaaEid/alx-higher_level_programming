5ac6bf1d6ccd3a9fdc93fbdf31f9760a957e9ac5
MonaaEid <monaa_eid@hotmail.com>
2023-10-11 00:54:50 +0300

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,8 +15,7 @@ def print_statistics(total_size, status_codes):
 
 def main():
     total_size = 0
-    status_codes = {'200': 0, '301': 0,
-    '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     count = 0
     try:
         for line in sys.stdin:
