b97feb12ca28306dd215032c67d15cafbc90686d
MonaaEid <monaa_eid@hotmail.com>
2023-10-10 18:20:37 +0300

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,5 @@ def read_file(filename=""):
     Args:
         filename: the file name
     """
-    with open('UTF8', encoding="utf-8") as f:
-    read_data = f.read()
-    print({}.format(read_data))
+    with open(filename, 'r') as file:
+        print(file.read())
