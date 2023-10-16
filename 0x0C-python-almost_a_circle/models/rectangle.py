1ff5603bc90cc76347b8356da3e5fcc9348c3441
MonaaEid <monaa_eid@hotmail.com>
2023-10-16 22:02:56 +0300

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,7 +93,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns a string representation of a Rectangle instance."""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle]({self.id}) {self.x}/{self.y}
+        - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
         """Updates a Rectangle instance with new attribute values."""
