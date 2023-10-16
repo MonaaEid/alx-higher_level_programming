56aeb2e07a01ab15a8aa18985404076dc166e5eb
MonaaEid <monaa_eid@hotmail.com>
2023-10-16 21:33:01 +0300

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 """
 Ractangle class
 """
-
-
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """A Square class.
 """
-
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Represent a Square class that inherits from Rectangle
     """
-
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
