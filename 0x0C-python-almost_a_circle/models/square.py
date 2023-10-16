d6d3496fb625ef4e6486dc8cf8a318edb0ee547b
MonaaEid <monaa_eid@hotmail.com>
2023-10-16 20:38:27 +0300

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """A Square class.
 """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Represent a Square class that inherits from Rectangle
     """
-
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square."""
@@ -24,35 +24,13 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        else:
+            for key in kwargs:
+                setattr(self, key, kwargs[key])
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
@@ -64,4 +42,5 @@ class Square(Rectangle):
         }
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
