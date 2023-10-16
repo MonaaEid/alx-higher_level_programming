dfbbe5d4ba84b02d1456c0e4e85a49d34bc16cc4
MonaaEid <monaa_eid@hotmail.com>
2023-10-16 21:50:33 +0300

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A Square class.
 """
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
@@ -40,5 +40,4 @@ class Square(Rectangle):
         }
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
