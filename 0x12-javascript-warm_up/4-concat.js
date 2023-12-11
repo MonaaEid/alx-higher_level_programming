18074e6760f0f26f17c02db6dab7641f17eb7fdc
MonaaEid <monaa_eid@hotmail.com>
2023-12-11 19:45:39 +0200

    test

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,5 +3,8 @@ const ar = process.argv;
 const _is = 'is';
 if (ar[2] && ar[3]) {
   console.log(ar[2], _is, ar[3]);
+} else if (ar[2] && !ar[3]) {
+  console.log(ar[2], _is, 'undefined');
+} else {
+  console.log('undefined', _is, 'undefined');
 }
-
