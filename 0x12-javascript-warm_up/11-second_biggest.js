bd309e837700401440f8ca53e61e3fc9210ecf6a
MonaaEid <monaa_eid@hotmail.com>
2023-12-12 00:42:11 +0200

    test

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,11 @@ const ar = process.argv;
 if (ar.length <= 3) {
   console.log(0);
 } else {
-    const args = process.argv.slice(2);
-    const sortedArr = args.map(Number).sort((a, b) => b - a);
-    console.log(sortedArr[1]);
+  const args = process.argv.slice(2);
+  const sortedArr = args.map(Number).sort((a, b) => b - a);
+  console.log(sortedArr[1]);
 
-    // return sortedArr[1] || 0;
+  // return sortedArr[1] || 0;
 //   for (let i = 2; i < ar.length; i++) {
 //     const element = Math.floor(parseFloat(ar[i]));
 //     if (!isNaN(element) && element > prevNum) {
