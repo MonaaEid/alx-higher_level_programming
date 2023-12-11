d4e82627aff12fe3fc89e5542702724b5f5368d6
MonaaEid <monaa_eid@hotmail.com>
2023-12-12 00:00:20 +0200

    test

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const ar = process.argv;
 let maxNum; let prevNum = 0;
-if (ar.length === 2 || ar.length === 3) {
+if (ar.length === 2) {
   console.log(0);
 } else if (ar.length === 3) {
-
+  console.log(0);
 } else {
   for (const key in ar) {
     const element = Math.floor(parseFloat(ar[key]));
