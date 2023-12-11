678385170922907f91f0621855a4807ec4891ada
MonaaEid <monaa_eid@hotmail.com>
2023-12-12 01:11:32 +0200

    test

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.function (x, theFunction); {
-    for (let i = 0; i < x; i++) {
-        theFunction();
-}
-}
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
