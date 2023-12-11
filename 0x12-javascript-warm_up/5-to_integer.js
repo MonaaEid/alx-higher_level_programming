e628bd60fa3489ce2d23d483fac5a18540651b77
MonaaEid <monaa_eid@hotmail.com>
2023-12-11 20:46:31 +0200

    test

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const ar = process.argv;
 if (Math.floor(parseFloat(ar[2]))) {
-  console.log('My number: ', ar[2]);
+  console.log('My number:', ar[2]);
 } else {
   console.log('Not a number');
 }
