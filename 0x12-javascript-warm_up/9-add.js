2c3b7e69b2a168be4f9cd5da78216261ecd79f04
MonaaEid <monaa_eid@hotmail.com>
2023-12-11 23:13:15 +0200

    test

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const ar = process.argv;
+const a = Math.floor(parseFloat(ar[2]));
+const b = Math.floor(parseFloat(ar[3]));
+if (!a || !b) {
+    console.log(NaN);
+} else {
+    console.log(a + b)
+}
