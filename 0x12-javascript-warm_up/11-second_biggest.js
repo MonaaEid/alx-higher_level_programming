3dd78b386e37b3624c2f9459e835bb1486e80957
MonaaEid <monaa_eid@hotmail.com>
2023-12-12 00:41:00 +0200

    test

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 const ar = process.argv;
-let maxNum;
-let prevNum = 0;
+
 if (ar.length <= 3) {
   console.log(0);
 } else {
-  for (let i = 2; i < ar.length; i++) {
-    const element = Math.floor(parseFloat(ar[i]));
-    if (!isNaN(element) && element > prevNum) {
-      maxNum = element;
-      prevNum = element;
-    }
-  }
-  console.log(maxNum);
+    const args = process.argv.slice(2);
+    const sortedArr = args.map(Number).sort((a, b) => b - a);
+    console.log(sortedArr[1]);
+
+    // return sortedArr[1] || 0;
+//   for (let i = 2; i < ar.length; i++) {
+//     const element = Math.floor(parseFloat(ar[i]));
+//     if (!isNaN(element) && element > prevNum) {
+//       maxNum = element;
+//       prevNum = element;
+//     }
+//   }
 }
