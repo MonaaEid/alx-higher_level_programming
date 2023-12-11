ab80ba664443dc08301b5eeb50b102b73bb285eb
MonaaEid <monaa_eid@hotmail.com>
2023-12-11 18:41:15 +0200

    test

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-// print process.argv
-  if (argv.length === 2) {
-    console.log('No argument');
-  }
-  else if ( argv.length === 3)
-  {
-    console.log('Argument found');
-  }
-  else
-  {
-    console.log('Arguments found');
-  }
-  // console.log(`${index}: ${val}`);
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
