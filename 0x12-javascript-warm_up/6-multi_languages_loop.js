152a203897d45b1ade23ec67509f1a1e19827938
MonaaEid <monaa_eid@hotmail.com>
2023-12-11 21:19:38 +0200

    test

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const item in arr) {
-  if (arr.hasOwnProperty.call(arr, item)) {
     const element = arr[item];
     console.log(element);
-  }
-} {
-
 }
