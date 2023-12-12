ba23485ab6a19f37f1eb92f6528dbf7dd54e04f1
MonaaEid <monaa_eid@hotmail.com>
2023-12-12 17:42:57 +0200

    test

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (h, w) {
+    this.height = h;
+    this.width = w;
+    
+    if (w, h <= 0) {
+      const myObject = {};
+  }
+  }
+};
