6b038688b34080884b2abd753fd3404e5fe18f64
MonaaEid <monaa_eid@hotmail.com>
2023-12-12 17:52:27 +0200

    test

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (h, w) {
-    this.height = h;
-    this.width = w;
-
-    if (w, h <= 0) {
-      const myObject = {};
+  constructor (w, h) {
+    if (w || h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 };
