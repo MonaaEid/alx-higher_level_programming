98f9a38d26fdc1a174032a37a5c5420ba6420339
MonaaEid <monaa_eid@hotmail.com>
2023-12-15 19:08:31 +0200

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             passwd=password,
             db=database, port=3306)
         cursor = db_connection.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+        cursor.execute("SELECT * FROM states WHERE name LIKE %s \
                     ORDER BY id ASC", (stateNameSearched,))
         states = cursor.fetchall()
         for state in states:
