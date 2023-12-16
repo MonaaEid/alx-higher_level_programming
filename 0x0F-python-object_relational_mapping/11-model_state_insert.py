32cb4b290ac542cbba3cb38ebb9bff2f21e0f088
MonaaEid <monaa_eid@hotmail.com>
2023-12-16 19:01:28 +0200

    update

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    state_new = State(name='Louisiana')
+    session.add(state_new)
+    session.commit()
+
+    print(state_new.id)
+    session.close()
