886f2ab6202e8bcf9abd5aac9ee5713882ce286b
MonaaEid <monaa_eid@hotmail.com>
2023-12-18 01:46:30 +0200

    update

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,16 +6,14 @@
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import (create_engine)
-# engine = create_engine("sqlite:///:memory:")
-# Base = declarative_base()
+
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """ class State"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,  nullable=False)
+    id = Column(Integer, unique=True, primary_key=True,  nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete', backref='state')
     # Base.metadata.create_all(engine)
