a0a2b6f764bb840bdd775a4213a8855fcbe56e1a
MonaaEid <monaa_eid@hotmail.com>
2024-01-05 20:58:46 +0200

    test

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -X OPTIONS "$1"
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -s -X OPTIONS "$1" -I | grep "Allow:" | cut -d " " -f2-
