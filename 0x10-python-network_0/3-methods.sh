0edd5121ddca0d919e6a4ab849a6df2dd2c35e91
MonaaEid <monaa_eid@hotmail.com>
2024-01-05 20:50:04 +0200

    test

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -s -X OPTIONS "$1"
