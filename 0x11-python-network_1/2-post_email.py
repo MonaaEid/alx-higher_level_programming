a4ba108816b3188ce65135123eedeae4bbc12806
MonaaEid <monaa_eid@hotmail.com>
2024-01-07 00:24:52 +0200

    test

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,13 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-email  = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email  = sys.argv[2]
 
-params = {"email": email}
-data = urllib.parse.urlencode(params).encode("utf-8")
+    params = {"email": email}
+    data = urllib.parse.urlencode(params).encode("utf-8")
 
-with urllib.request.urlopen(url, data) as response:
-    body = response.read()
-    print(body.decode("utf-8"))
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+        print(body.decode("utf-8"))
