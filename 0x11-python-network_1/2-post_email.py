976565c058a15da63f67985bad102fb399a53b1b
MonaaEid <monaa_eid@hotmail.com>
2024-01-07 00:27:21 +0200

    test

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email  = sys.argv[2]
+    email = sys.argv[2]
 
     params = {"email": email}
     data = urllib.parse.urlencode(params).encode("utf-8")
