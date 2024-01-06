8f2bebe809769681bacb5a1c8820ab8c896d8ecd
MonaaEid <monaa_eid@hotmail.com>
2024-01-06 23:35:38 +0200

    test

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""fetches X-Request-Id."""
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+email  = sys.argv[2]
+
+params = {"email": email}
+data = urllib.parse.urlencode(params).encode("utf-8")
+
+with urllib.request.urlopen(url, data) as response:
+    body = response.read()
+    print(body.decode("utf-8"))
