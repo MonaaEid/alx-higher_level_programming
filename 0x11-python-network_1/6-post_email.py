0cd55180c40bd8ea56fa3560b90f3ac564ac786e
MonaaEid <monaa_eid@hotmail.com>
2024-01-07 19:53:49 +0200

    update

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""fetches X-Request-Id in the response header"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    params = {"email": email}
+
+    # response = requests.get(url)
+    response = requests.post(url, data=params)
+    print(response.text)
