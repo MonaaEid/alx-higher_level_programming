ad006796264248312a05366a54d688b304b7e77a
MonaaEid <monaa_eid@hotmail.com>
2024-01-07 20:16:40 +0200

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         response = requests.get(url)
-        response.raise_for_status()
+        print(response.text)
 
     except HTTPError as http_err:
         print("Error code: {}".format(http_err.code))
