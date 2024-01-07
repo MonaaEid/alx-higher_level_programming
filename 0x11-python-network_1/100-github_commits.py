54f6e9cfb790fca712dbaa1d1b402fb942d1abad
MonaaEid <monaa_eid@hotmail.com>
2024-01-08 00:41:02 +0200

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,28 +2,17 @@
 import requests
 import sys
 
-# Get the repository name and the owner name from the command line arguments
+
 repo = sys.argv[1]
 owner = sys.argv[2]
 
-# Set the URL for the GitHub API
 url = f"https://api.github.com/repos/{owner}/{repo}"
 
-# Make a GET request to the URL
 response = requests.get(url)
 
-# Check if the request was successful
+
 if response.status_code == 200:
-    # Parse the JSON data from the response
     data = response.json()
 
     # Print the repository information
-    print("Repository name:", data["name"])
-    print("Repository description:", data["description"])
-    print("Repository language:", data["language"])
-    print("Repository stars:", data["stargazers_count"])
-    print("Repository forks:", data["forks_count"])
-else:
-    # Print the error message
-    print("Something went wrong:", response.reason)
-
+    print("{}: {}",data["author"], data["name"] )
