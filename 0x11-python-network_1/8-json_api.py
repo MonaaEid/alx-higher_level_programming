f569ccc6474fa627e9a5a823e930eacd961775a5
MonaaEid <monaa_eid@hotmail.com>
2024-01-08 00:29:52 +0200

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     params = {"q": q}
     response = requests.post(url, data=params)
-    response = requests.get(url)
     try:
         json = response.json()
         if not json:
