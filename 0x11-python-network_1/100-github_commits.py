3c7ecb4a0536288f7c0986410c3adf26d9dec534
MonaaEid <monaa_eid@hotmail.com>
2024-01-08 02:17:03 +0200

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,15 +8,17 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
 
-    url = f"https://api.github.com/repos/{owner}/{repo}"
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 
     response = requests.get(url)
 
 
     if response.status_code == 200:
-        data = response.json()
-        for i in data[:10]:
-            # Print the repository information
-            print("{}: {}",data["author"], data["name"] )
-    # print("{}: {}".format(i.get('sha'),
-    #                                 i.get('commit').get('author').get('name')))
+        # data = response.json()
+        # for i in response.json()[:10]:
+            # print("{}: {}",i["sha"], i["name"])
+            # print("{}: {}".format(i.get('sha'),
+            #                     i.get('commit').get('author').get('name')))
+        for i in response.json()["commits"][:10]:
+            # Print the commit author and sha
+            print(f" {i['sha']}: {i['commit']['author']['name']}")
