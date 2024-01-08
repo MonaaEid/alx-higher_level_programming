534b41d043dd0b32ef01ed1280e22f3d9961de38
MonaaEid <monaa_eid@hotmail.com>
2024-01-08 02:20:17 +0200

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,13 +12,6 @@ if __name__ == "__main__":
 
     response = requests.get(url)
 
-
     if response.status_code == 200:
-        # data = response.json()
-        # for i in response.json()[:10]:
-            # print("{}: {}",i["sha"], i["name"])
-            # print("{}: {}".format(i.get('sha'),
-            #                     i.get('commit').get('author').get('name')))
-        for i in response.json()["commits"][:10]:
-            # Print the commit author and sha
-            print(f" {i['sha']}: {i['commit']['author']['name']}")
+        for data in response.json()[:10]:
+            print(f" {data['sha']}: {data['commit']['author']['name']}")
