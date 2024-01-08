eeae96b67fc25999cbbdb0e6c0862ba014cefbe1
MonaaEid <monaa_eid@hotmail.com>
2024-01-08 02:02:43 +0200

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,17 +4,19 @@ import requests
 import sys
 
 
-repo = sys.argv[1]
-owner = sys.argv[2]
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
 
-url = f"https://api.github.com/repos/{owner}/{repo}"
+    url = f"https://api.github.com/repos/{owner}/{repo}"
 
-response = requests.get(url)
+    response = requests.get(url)
 
 
-if response.status_code == 200:
-    data = response.json()
-    for i in data[:10]:
-        # Print the repository information
-        print("{}: {}".format(i.get('sha'),
-                              i.get('commit').get('author').get('name')))
+    if response.status_code == 200:
+        data = response.json()
+        for i in data[:10]:
+            # Print the repository information
+            print("{}: {}",data["author"], data["name"] )
+    # print("{}: {}".format(i.get('sha'),
+    #                                 i.get('commit').get('author').get('name')))
