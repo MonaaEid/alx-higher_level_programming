8a382a0544bb86f565e46764bc6c32f4a23f4774
MonaaEid <monaa_eid@hotmail.com>
2024-01-30 20:15:38 +0200

    test

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,9 +7,11 @@ const characterId = 18;
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
+  } else if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
     const numFilms = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
     console.log(numFilms);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
