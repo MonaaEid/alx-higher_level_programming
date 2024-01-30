4d9b21f7b913aa33c1fdd1710f87a7469fceb1a8
MonaaEid <monaa_eid@hotmail.com>
2024-01-30 19:58:45 +0200

    test

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}`, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
